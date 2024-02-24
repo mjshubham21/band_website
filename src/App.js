e272ff9a1a6982e72ae305c04cc0dc6837c9be0e
Muskaan Shaikh <ymuskaanshaikh0440@gmail.com>
2024-02-24 21:14:22 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import Navigation from './Navigation';
 import Header from './Header';
+import  Band from './Band';
 import './App.css';
+import './para.css'
 
 function App() {
   const data = [
-    {home:"Home", band:'Band', tour:"Tour", contact : 'Contact'}
+    {home:"Home", band:'Band', tour:"Tour", contact : 'Contact'},
+    {head:"THE BAND" ,span:"We love Music", para:"we have create a fictional band Website. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus quibusdam, facilis iste laboriosam rem reprehenderit non nesciunt impedit earum officia exercitationem consequatur magnam maiores quidem, est modi ipsum odio explicabo. "},
+
   ];
   return (
     <div>
       <Navigation value={data[0].home} valueone={data[0].band} valuetwo={data[0].tour} valuethree={data[0].contact}/>
       <Header/>
+      <Band hone={data[1].head} sone={data[1].span} pone={data[1].para}/>
+      
     </div>
   );
 };
---
+++ src/para.js
@@ -0,0 +1,8 @@
+// import './para.css';
+// function para(props){
+//     return (
+//         <div className='para'>
+//             <h>THE BAND</h>
+//         </div>
+//     );
+// }
