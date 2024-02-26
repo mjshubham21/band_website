238e292204cc28c425e624ba10645edc8f338010
mjshubham21 <mjshubhampawar21@gmail.com>
2024-02-27 00:51:13 +0530

    Mj Card changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import './Card.css';
-import img3 from '../images/img3.jpg';
 const Card = (props) => {
-  console.log(props);
+  console.log("Data from Card Component :: ",props.data);
+  const data = props.data;
   return (
     <> {/* React Fragment */}
-      <div className='card-container'>
-        <div className="image"> 
-        <img src={img3} alt="" /></div>
-        <h3>NEW YORK</h3>
-        <p>18-Oct-2002</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem praesentium id maxime?</p>
-        <div className="btn">Buy Tickets.</div>
-      </div>
+      {
+        data.map((item, index) => {
+        return (
+          <div className='card-container' key={index}>
+            <img src={item.image} alt='city' className='card_img' />
+            <div className='card_info'>
+              <h2>{item.city}</h2>
+              <h6>{item.date}</h6>
+              <p>{item.description}</p>
+            </div>
+            <div className="btn">Buy Tickets</div>
+          </div>
+        )
+      })
+      }
     </>
   )
 }
--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import './Tour.css';
 import Card from './Card';
+import img3 from '../images/img3.jpg';
+import img4 from '../images/img4.jpg';
+import img5 from '../images/img5.jpg';
 
 const Tour = () => {
   const data =[{
+    image: img3,
     city: 'New York',
     date: '18-Oct-2002',
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem praesentium id maxime?'
   },
   {
+    image: img4,
     city: 'Los Angeles',
     date: '20-Oct-2002',
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem praesentium id maxime?'
   },
   {
+    image: img5,
     city: 'Chicago',
     date: '22-Oct-2002',
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem praesentium id maxime?'
-  
-  }]
+  },
+]
   return (
     <div className='tour'>
       <h1 className='tour1'>TOUR DATES</h1>
@@ -28,7 +34,6 @@ const Tour = () => {
       <p className='tour4'>September <span className='tour3'>Sold out</span></p>
       <Card data={data} />
     </div>
-      
   )
 };
 
